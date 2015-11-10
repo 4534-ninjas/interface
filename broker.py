718ece91867fc17465a99f4a71ba1babaf056c8b
Jean-Philippe Ouellet <jpo@vt.edu>
2015-11-09 21:10:31 -0500

    Initial protocol translation in message broker.

--- broker.py
+++ broker.py
@@ -8,8 +8,10 @@ import json
 import signal
 import SocketServer
 import threading
+import re
+import struct
 
-unix_addr = '/tmp/sock'
+unix_addr = 'sock'
 tcp_addr = ('0.0.0.0', 4321)
 
 try:
@@ -24,24 +26,85 @@ tcp_lock = threading.Lock()
 unix_clients = set()
 unix_lock = threading.Lock()
 
-def tcp_to_unix(line):
-	return line
+class tcp_pkt_iter:
+	def __init__(self, sock):
+		#self.re = re.compile(r'[^\xff]*(?:\xff})?\xff{([^\xff]*)\xff}(.*)')
+		self.re = re.compile(r'([^\n]*)\n(.*)')
+		self.sock = sock
+		self.buf = ""
 
-def unix_to_tcp(line):
-	return line
+	def __iter__(self):
+		return self
+
+	def getmore(self):
+		data = self.sock.recv(4096)
+		if not data:
+			raise StopIteration()
+		self.buf += data
+
+	def next(self):
+		while not self.re.match(self.buf):
+			self.getmore()
+		match = self.re.match(self.buf)
+		self.buf = match.group(2)
+		return match.group(1)
+
+def tcp_to_unix(msg, sock):
+	peer = sock.getpeername()
+	print 'from {}: {}' % (peer, msg)
+	x = {'from': peer, 'raw': msg}
+	if msg[0] == '\x01':
+		x['type'] = 'def'
+	elif msg[0] == '\x02':
+		x['type'] = 'msg'
+	else:
+		x['type'] = 'unknown'
+	return json.dumps(x)
+
+def unix_to_tcp(msg, sock):
+	print 'from {}: {}' % (sock.getpeername(), msg)
+	x = json.loads(msg)
+	return x['raw']
+
+def broadcast_tcp(msg):
+	with tcp_lock:
+		for s in tcp_clients:
+			s.sendall(msg)
+
+def broadcast_unix(msg):
+	with unix_lock:
+		for s in unix_clients:
+			s.sendall(msg)
+
+def update_rovers_present():
+	with tcp_lock:
+		def pack(sock):
+			peer = sock.getpeername()
+			return {'host': peer[0], 'port': peer[1]}
+		rovers = map(pack, tcp_clients)
+		print 'rovers present: '+str([s.getpeername() for s in tcp_clients])
+		broadcast_unix(json.dumps({'type': 'rover_list', 'rovers': rovers}))
 
 class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
 	def handle(self):
+		print 'tcp connected'
 		with tcp_lock:
 			tcp_clients.add(self.request)
-		for line in iter(self.request.makefile().readline, ''):
-			print line
-			forward = tcp_to_unix(line)
-			with unix_lock:
-				for s in unix_clients:
-					s.sendall(forward)
+		update_rovers_present()
+		for pkt in iter(tcp_pkt_iter(self.request)):
+			broadcast_unix(tcp_to_unix(pkt, self.request))
+		self.on_done()
+
+	def handle_timeout(self):
+		self.on_done()
+
+	def handle_timeout(self):
+		self.on_done()
+
+	def on_done(self):
 		with tcp_lock:
 			tcp_clients.remove(self.request)
+		update_rovers_present()
 		print 'tcp disconnected'
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
@@ -49,14 +112,12 @@ class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
 
 class ThreadedUnixStreamRequestHandler(SocketServer.BaseRequestHandler):
 	def handle(self):
+		print 'unix connected'
 		with unix_lock:
 			unix_clients.add(self.request)
 		for line in iter(self.request.makefile().readline, ''):
-			print line
-			forward = unix_to_tcp(line)
-			with tcp_lock:
-				for s in tcp_clients:
-					s.sendall(forward)
+			# maybe catch exceptions here & notify sender
+			broadcast_tcp(unix_to_tcp(line, self.request))
 		with unix_lock:
 			unix_clients.add(self.request)
 		print 'unix disconnected'
@@ -65,6 +126,7 @@ class ThreadedUnixStreamServer(SocketServer.ThreadingMixIn, SocketServer.UnixStr
 	pass
 
 if __name__ == "__main__":
+	SocketServer.TCPServer.allow_reuse_address = True
 	tcp_server = ThreadedTCPServer(tcp_addr, ThreadedTCPRequestHandler)
 	ip, port = tcp_server.server_address
 	print 'tcp: %s:%d' % (ip, port)
@@ -79,9 +141,12 @@ if __name__ == "__main__":
 	unix_server_thread.daemon = True
 	unix_server_thread.start()
 
-	signal.pause()
+	def sigh(a,b):
+		#tcp_server.shutdown()
+		#unix_server.shutdown()
+		tcp_server.server_close()
+		unix_server.server_close()
+		os._exit(0)
+	signal.signal(signal.SIGINT, sigh)
 
-	tcp_server.shutdown()
-	tcp_server.server_close()
-	unix_server.shutdown()
-	unix_server.server_close()
+	signal.pause()
