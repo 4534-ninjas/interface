cf92a43bbbf07327ac7cf8aabbf6daa395be5206
Jean-Philippe Ouellet <jpo@vt.edu>
2015-11-10 05:15:31 -0500

    Enumerate message info when first seen.
    So we can get all new message types as they show up without
    polling for them.

--- debug/debug.c
+++ debug/debug.c
@@ -85,6 +85,50 @@ vfmtlen(const char *simple_fmt, va_list src_ap)
 }
 
 void
+debug_dump(struct debug_entry *e)
+{
+#ifdef PKT_DEBUG
+	// this is all kinda ugly :/
+	struct vals {
+		uint8_t type;
+		uint32_t e;
+		uint32_t count;
+		uint32_t line;
+		uint8_t enabled;
+	} __attribute__((__packed__));
+	size_t file_len = strlen(e->file) + 1;
+	size_t fmt_len = strlen(e->fmt) + 1;
+	size_t sfmt_len = strlen(e->simple_fmt) + 1;
+	size_t buf_len = sizeof(struct vals) + file_len + fmt_len + sfmt_len;
+	char *buf = alloca(buf_len);
+	struct vals *v = (struct vals *)buf;
+	v->type = 'D'; // `Definition` msg type.
+	v->e = htonl((uint32_t)(intptr_t)e);
+	v->count = htonl((uint32_t)e->count);
+	v->line = htonl((uint32_t)e->line);
+	v->enabled = (uint8_t)e->enabled;
+	char *p = buf + sizeof(struct vals);
+	memcpy(p, e->file, file_len); p += file_len;
+	memcpy(p, e->fmt, fmt_len); p += fmt_len;
+	memcpy(p, e->simple_fmt, sfmt_len); p += sfmt_len;
+	assert(p - buf == buf_len); // remove me?
+	send_pkt(1, buf, buf_len);
+#else
+	printf("%zx\t%sabled\t%lu\t%s:%ld\t%s\n",
+	    (size_t)e, e->enabled ? "en" : "dis",
+	    e->count, e->file, e->line, e->fmt);
+#endif
+}
+
+void
+debug_dump_all(void)
+{
+	struct debug_entry *e;
+	for (e = head; e != NULL; e = e->next)
+		debug_dump(e);
+}
+
+void
 __debug(struct debug_entry *e, const char *file, long line, const char *fmt, ...)
 {
 	if (e->file == NULL) {
@@ -95,6 +139,7 @@ __debug(struct debug_entry *e, const char *file, long line, const char *fmt, ...
 		e->simple_fmt = simplify(fmt);
 		e->next = head;
 		head = e;
+		debug_dump(e); // Broadcast type info when first seen.
 	}
 	e->count++;
 
@@ -155,50 +200,6 @@ __debug(struct debug_entry *e, const char *file, long line, const char *fmt, ...
 		va_end(ap);
 #endif
 	}
-}
-
-void
-debug_dump(struct debug_entry *e)
-{
-#ifdef PKT_DEBUG
-	// this is all kinda ugly :/
-	struct vals {
-		uint8_t type;
-		uint32_t e;
-		uint32_t count;
-		uint32_t line;
-		uint8_t enabled;
-	} __attribute__((__packed__));
-	size_t file_len = strlen(e->file) + 1;
-	size_t fmt_len = strlen(e->fmt) + 1;
-	size_t sfmt_len = strlen(e->simple_fmt) + 1;
-	size_t buf_len = sizeof(struct vals) + file_len + fmt_len + sfmt_len;
-	char *buf = alloca(buf_len);
-	struct vals *v = (struct vals *)buf;
-	v->type = 'D'; // `Definition` msg type.
-	v->e = htonl((uint32_t)(intptr_t)e);
-	v->count = htonl((uint32_t)e->count);
-	v->line = htonl((uint32_t)e->line);
-	v->enabled = (uint8_t)e->enabled;
-	char *p = buf + sizeof(struct vals);
-	memcpy(p, e->file, file_len); p += file_len;
-	memcpy(p, e->fmt, fmt_len); p += fmt_len;
-	memcpy(p, e->simple_fmt, sfmt_len); p += sfmt_len;
-	assert(p - buf == buf_len); // remove me?
-	send_pkt(1, buf, buf_len);
-#else
-	printf("%zx\t%sabled\t%lu\t%s:%ld\t%s\n",
-	    (size_t)e, e->enabled ? "en" : "dis",
-	    e->count, e->file, e->line, e->fmt);
-#endif
-}
-
-void
-debug_dump_all(void)
-{
-	struct debug_entry *e;
-	for (e = head; e != NULL; e = e->next)
-		debug_dump(e);
 }
 
 int
