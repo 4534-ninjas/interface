c32f9c919f133676b3a9ff9c905150f6111f082f
Jean-Philippe Ouellet <jpo@vt.edu>
2015-11-10 17:40:51 -0500

    Increase tested output length & add null test.

--- debug/rover_tests.sh
+++ debug/rover_tests.sh
@@ -4,6 +4,6 @@ d='rover_tests'
 for x in "$d"/in_*; do
 	i=$(echo "$x" | sed 's/.*_//')
 	echo "Running test $i..."
-	(sleep 1; cat $x) | ./rover 2>/dev/null | head -c 100 > "$d"/out_$i
+	(sleep 1; cat $x) | ./rover 2>/dev/null | head -c 1000 | tee "$d"/out_$i | hexdump -C
 	diff -ur "$d"/good_$i "$d"/out_$i
 done
