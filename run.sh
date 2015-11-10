e5a8e769df379d5545db280a6530cb58786cdb09
Jean-Philippe Ouellet <jpo@vt.edu>
2015-11-09 21:12:04 -0500

    run.sh: Support websocketd "dev console".

--- run.sh
+++ run.sh
@@ -3,9 +3,14 @@
 websocketd=/home/jpo/go/bin/websocketd
 PORT=8080
 
+if [ "X$1" = Xdev ]; then
+	static='--devconsole'
+else
+	static='--staticdir=static --cgidir=cgi'
+fi
+
 cd "$(dirname "$0")"
 exec $websocketd \
   --port=$PORT \
-  --dir=websocket \
-  --staticdir=static \
-  --cgidir=cgi
+  $static \
+  --dir=websocket
